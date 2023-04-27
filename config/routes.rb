8e4688c9231ba919e659b982619f646126e7b2e8
dotaplayer1 <georgemao72@gmail.com>
2023-04-26 17:19:06 -0700

    Add delete and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "articles#index"
 
+  resources :articles do
+    resources :comments
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
